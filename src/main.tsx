ef69b32cc9ca6676ed13447254124606d7100f98
Ali Bin_Dohri <alibin_dohri@Alis-MacBook-Pro-2.local>
2024-08-19 08:55:07 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import App from './App.tsx'
+import './index.css'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+)
